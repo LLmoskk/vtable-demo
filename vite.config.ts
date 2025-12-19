ab025e1c37ee848d9a90d0d18f6a9a7e3daff239
LLmoskk <1398145450@qq.com>
2025-12-19 10:27:36 +0800

    feat: 集成tailwind与shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     base: process.env.NODE_ENV === 'production' ? '/vtable-demo/' : '/',
     build: {
       outDir: 'dist',
